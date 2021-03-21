b55fd5920e33c63437013d1fb50c38abda9e3431
Aline <alinealexandre99@gmail.com>
2021-03-21 20:20:00 +0100

    New interface add

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -34,6 +34,8 @@
                     <tr>
                     <td>
                         <input type="checkbox" name="emp_liq" id="emp_liq" onclick="habilitar()" />
+                            
+                        
                         </td>
                         
                         
