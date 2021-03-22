9bcdeebc0fe45b69e39fe397890df9630f55c519
Aline <alinealexandre99@gmail.com>
2021-03-22 04:59:12 +0100

    Form.blade modification

--- resources/views/products/form.blade.php
+++ resources/views/products/form.blade.php
@@ -26,6 +26,12 @@
                         <input type="text" name ='name' class="form-control" value="{{$product->name}}" >
                     </div>
                     
+                    
+                    <div class="form-check">
+                    <input name="check" type="checkbox" class="form-check-input" value="1" >
+                      <label class="form-check-label" >Check</label>
+                    </div>
+
                     <button type="submit" class="btn btn-primary">Bekijk</button>
                     </form>
 
@@ -36,6 +42,12 @@
                     <div class="form-group">
                         <label for="exampleInputProduct">Product Naam  </label>
                         <input type="text" name ='name' class="form-control" >
+                        
+                    </div>
+
+                    <div class="form-check">
+                      <input type="checkbox" class="form-check-input" id="check">
+                      <label class="form-check-label" >Check</label>
                     </div>
 
                     <button type="submit" class="btn btn-primary">Bekijk</button>
